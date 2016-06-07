1fc8a7e51f89b68cd0be9548fcc78380f03dee2e
Vitaliy Emelianov <Vitaliy030589@gmail.com>
2016-06-07 07:32:33 +0400

    making main page 60% finished

---
+++ content/dist/js/app.js
@@ -0,0 +1,12 @@
+window.moffConfig = {
+	// Website CSS breakpoints.
+	// Default values from Twitter Bootstrap.
+	// No need to set xs, because of Mobile first approach.
+	breakpoints: {
+		sm: 640,
+		md: 992,
+		lg: 1200
+	},
+	loadOnHover: true,
+	cacheLiveTime: 2000
+};
