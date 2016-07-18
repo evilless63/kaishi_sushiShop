f1e561579edd7ac022e58070238da6e0b971d32e
Vitaliy Emelianov <Vitaliy030589@gmail.com>
2016-07-18 22:01:16 +0400

    90% desktop

--- content/dist/js/app.js
+++ content/dist/js/app.js
@@ -28,4 +28,64 @@ $(document).ready(function(){
 		function(){
 			$( ".registrationWrapper" ).css( "display", "none" );
 		});
+	
+	$( ".sushiBlockGetBusket" ).click(
+		function(){
+			$( ".sushiProfailWrapper" ).css( "display", "block" );	
+		});
+	$( ".closeModal" ).click(
+		function(){
+			$( ".sushiProfailWrapper" ).css( "display", "none" );
+		});	
+	
+	$(".sushiBlockGetBusketProfail").click(
+		function(){
+			alert("Товар добавлен в корзину !");
+		});	
+
+	// $(".addCommentButton").click(
+	// 	function(){
+	// 		if($(".addCommentField").hasClass("hidden")){
+	// 			$(this).removeClass("hidden").addClass("block");
+	// 		} else {
+	// 			$(this).removeClass("block").addClass("hidden");
+	// 		}
+	// 	});
+
+	$(".addCommentButton").click(function(){
+		$(".addCommentField").css("display", "block");
+	});	
+
+
+
+	//cart page input value
+
+	// $(".cartProductsBlockCountMinus").click(function(){
+	// 	var inputValue = $(this).parent("cartProductsBlockCount").find("cartProductsBlockInputcount").val();
+	// 	if(inputValue > "1") {
+	// 		inputValue = inputValue --;
+	// 	} else {
+	// 		alert ("Выбрано минимальное количество товара !");
+	// 	}
+	// })
+
+
+	$(".spoiler").spoiler({includePadding: false});
+
 });
+
+(function($) {
+$(function() {
+
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+});
+})(jQuery);
+
+
+
+
